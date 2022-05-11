0e392aff88ed26476485c1d2f02fa65be6049906
Jamesferro76 <jamesferromusic@gmail.com>
2022-05-11 17:34:45 -0600

    Objective2Lab3 complete

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1=2;
+    double num2=4;
+    double num3=6;
+    double average= (num1+num2+num3)/3;
+    System.out.println(average);
+
+  }
+}
