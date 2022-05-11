7f627106765123ad624230d77cafebaee3fd928b
Jamesferro76 <jamesferromusic@gmail.com>
2022-05-11 17:27:11 -0600

    Objective2Lab2 complete

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,17 @@
+public class Objective2Lab2 {
+
+  public static void main(String[] args) {
+    String name = "James Ferro";
+    int age = 28;
+    String  state = "New York";
+
+    System.out.print("Name: ");
+    System.out.print(name);
+    System.out.print(" Age: ");
+    System.out.print(age);
+    System.out.print(" State: ");
+    System.out.print(state);
+
+
+  }
+}
