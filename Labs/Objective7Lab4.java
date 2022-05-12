bf18d04778f5b44483d63941b7e266b9c9a12df8
Jamesferro76 <jamesferromusic@gmail.com>
2022-05-12 12:46:58 -0600

    Complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int num=1;
+    int sum=0;
+
+    while(num<=20){
+      sum=sum+num;
+      num=num+1;
+    }
+    System.out.println(sum);
+  }
+}
